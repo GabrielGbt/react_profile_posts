7cde700f3c963993feb78eb1603ac1abc0b22cdd
GabrielGbt <82351304+GabrielGbt@users.noreply.github.com>
2024-09-26 09:02:15 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const posts = [
     author: {
       avatarUrl: 'https://plus.unsplash.com/premium_photo-1679397743664-f9384e6ad49d?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       name: 'Primo rico',
-      role: 'dono da porsche',
+      role: 'ator',
     },
     content: [
       { type: 'paragraph', content: 'Fala galera' },
